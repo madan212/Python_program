bf62eb157eaec84b253d874c2e7e273150841c79
madan <madanmadhu212@gmail.com>
2021-03-16 13:54:25 +0530

    first commit

---
+++ algorithms/vampire.py
@@ -0,0 +1,11 @@
+name = 'carol'
+age = 3000
+if name == 'Alice':
+    print('Hi, Alice')
+elif age<12:
+    print('you are not Alice, kiddo.')
+elif age>2000:
+    print("unlike you,Alice is not an undead, immortal vampire.")
+elif age>100:
+    print('you are not Alice, grannie.')
+    
---
+++ algorithms/vampire2.py
@@ -0,0 +1,13 @@
+name = input()
+age = int(input())
+if name == 'Alice':
+    print('Hi, Alice')
+elif age<12:
+    print('you are not Alice, kiddo.')
+elif age>2000:
+    print("unlike you,Alice is not an undead, immortal vampire.")
+elif age>100:
+    print('you are not Alice, grannie.')
+else:
+    print('ypu are neither Alic nor a little kid')
+    
